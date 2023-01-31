0646cf7893aa386d00ec5715694b7e7de4ac5f3d
Milosska <110258611+Milosska@users.noreply.github.com>
2023-01-31 22:10:06 +0200

    disable animation on phones

--- js/aos.js
+++ js/aos.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+const aosArray = document.querySelectorAll('[data-aos]');
+console.log(window.screen.availWidth);
+
+function disableAos() {
+  if (window.screen.availWidth <= 480) {
+    aosArray.forEach(elem => elem.removeAttribute('data-aos'));
+  }
+}
+
+disableAos();
